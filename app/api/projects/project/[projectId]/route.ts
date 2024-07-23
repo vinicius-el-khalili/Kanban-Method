d6c802865e441618a7602340fa7fbe7a669f3413
vini <velkhaliliborsato@gmail.com>
2024-07-23 19:07:57 -0300

    GET /api/projects/project/[projectId]

---
+++ app/api/projects/project/[projectId]/route.ts
@@ -0,0 +1,14 @@
+import Project from "@/models/Project";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req:NextRequest,{params}:{params:{projectId:string}}){
+    try{
+
+        const {projectId} = params
+        const project = await Project.findById(projectId)
+        return NextResponse.json(project)
+
+    }catch(err:any){
+        return NextResponse.json({error:err.message},{status:500})
+    }
+}
