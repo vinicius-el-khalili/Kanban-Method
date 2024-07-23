3d2b7c51d05a6fcf876474414119b3bf0da2d2dd
vini <velkhaliliborsato@gmail.com>
2024-07-23 00:56:13 -0300

    post task route

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,14 +1,39 @@
 import dbConnect from "@/lib/dbConnect";
 import Task from "@/models/Task";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
+function tryCatch (f:any) {
+    try{ 
+        f()    
+    }catch(err:any){
+        return NextResponse.json({error:err.message})
+    }
+}
 
 export async function GET() {
     await dbConnect()
     try{ 
         const tasks = await Task.find({})    
-        return NextResponse.json(tasks)    
+        return NextResponse.json(tasks)   
     }catch(err:any){
-        return NextResponse.json({error:"error"})
+        return NextResponse.json({error:err.message})
+    }
+}
+
+export async function POST(req:NextRequest) {
+    await dbConnect()
+    try{ 
+        
+        const {title,description,status}:{
+            title:string,
+            description:string|undefined,
+            status:number
+        } = await req.json()
+        
+        const task = await Task.create({title,description,status})
+        return NextResponse.json({task},{status:200})
+
+    }catch(err:any){
+        return NextResponse.json({error:err.message})
     }
 }
