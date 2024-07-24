c1d9c489e58ba482f52a3f9f5adb1de8ed999306
vini <velkhaliliborsato@gmail.com>
2024-07-24 17:10:56 -0300

    initiating new Project model

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -2,24 +2,34 @@
 
 import { useProjectStore } from "@/store/Projects/ProjectStore";
 import ProjectCard from "./ProjectCard";
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import { useAuthStore } from "@/store/Auth/AuthStore";
+import { useEffect } from "react";
 
 const Projects = () => {
 
+    const authenticated = useAuthStore((state)=>(state.store.authenticated))
     const projects = useProjectStore((state)=>(state.store.projects))
     const refresh = useProjectStore((state)=>(state.method.refresh))
     const create = useProjectStore((state)=>(state.method.create))
+
+    useEffect(()=>{
+        if(!authenticated){ return }
+        refresh()
+    },[])
 
     return (
         <>
         <Button variant="contained" onClick={refresh}>refresh projects</Button>
         <Button variant="contained" onClick={()=>create("Peach's Project 2")}>create project</Button>
-        {
-        projects&&
-        projects.map((project,i)=>(
+        <Typography variant="h5">Projects</Typography>
+        {projects&&projects.map((project,i)=>(
             <ProjectCard key={`prj_crd${i}`} {...{project}}/>
-        ))
-        }
+        ))}
+        <Typography variant="h5">Shared Projects</Typography>
+        {projects&&projects.map((project,i)=>(
+            <ProjectCard key={`prj_crd${i}`} {...{project}}/>
+        ))}
         </>
     );
 }
--- models/Project.ts
+++ models/Project.ts
@@ -1,4 +1,4 @@
-import { Document, ObjectId, Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export type ProjectSchema = {
     user_id: string,
