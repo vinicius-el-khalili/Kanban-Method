4541bd20f8f37b764975cc3e2bcb3b4633bed6d9
vini <velkhaliliborsato@gmail.com>
2024-07-25 13:42:37 -0300

    checkpoint: Mininal Viable Product

--- app/u/project/[project_id]/page.tsx
+++ app/u/project/[project_id]/page.tsx
@@ -4,14 +4,12 @@ import Taeko from "@/components/layout/grids&layouts/Taeko";
 import CreateTask from "@/components/tasks/CreateTask";
 import KanbanGrid from "@/components/tasks/KanbanGrid";
 import { useProjectStore } from "@/store/Projects/ProjectStore";
-import { Box, Stack, Typography } from "@mui/material";
 import { useEffect } from "react";
 
 const Page = ({ params }: { params: { project_id: string } }) => {
 
     const { project_id } = params
     const selectedProject = useProjectStore((state)=>(state.store.selectedProject))
-    const selectProject = useProjectStore((state)=>(state.method.select))
     const initializeProjectPageByID = useProjectStore((state)=>(state.method.initializeProjectPageByID))
 
     useEffect(()=>{
