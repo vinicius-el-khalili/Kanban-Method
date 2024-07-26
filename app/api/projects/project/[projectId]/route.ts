73dc29dbd63d4fab907ffbb855233e71f680c88d
vini <velkhaliliborsato@gmail.com>
2024-07-26 18:04:04 -0300

    avoid duplicate project contributors

--- app/api/projects/project/[projectId]/route.ts
+++ app/api/projects/project/[projectId]/route.ts
@@ -21,8 +21,20 @@ export async function PATCH(req:NextRequest,{params}:{params:{projectId:string}}
     try{
 
         const {projectId} = params
-        
         const updatedProject:ProjectSchema&MongoDocument = await req.json()
+        const currentProject:ProjectSchema&MongoDocument|null = await Project.findById(updatedProject._id)
+        if (!currentProject){
+            return NextResponse.json("Project not found",{status:400})
+        }
+        if(updatedProject.user_id!=currentProject.user_id){
+            return NextResponse.json("can't update user id",{status:401})
+        }
+
+        // check for duplicated contributors
+        const updatedContributors = currentProject.contributors.map(c=>c.user_id)
+        if(new Set(updatedContributors).size != updatedContributors.length){
+            return NextResponse.json("contributor already added",{status:402})
+        }
 
         const project = await Project.updateOne({_id:projectId},{$set:updatedProject})
         return NextResponse.json(project)
