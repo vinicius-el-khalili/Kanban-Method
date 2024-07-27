d1f48d3e17da9e5397ac85e40d70d4965dff1819
vini <velkhaliliborsato@gmail.com>
2024-07-27 01:10:03 -0300

    fix add contributor

--- fetchers/getContributorByLogin.ts
+++ fetchers/getContributorByLogin.ts
@@ -1,12 +1,17 @@
 import { Contributor } from "@/models/Project"
+import { useAuthStore } from "@/store/Auth/AuthStore"
+import { NextResponse } from "next/server"
 
 export const getContributorByLogin = async (login:string) => {
 
+    const token = useAuthStore.getState().store.token
+    if(!token){ return false }    
     const res = await fetch(`/api/users/search/${login}`,{
         method:"GET",
         headers:{
             "Accept":"application/json",
             "Content-Type":"application/json",
+            "Authorization":`Bearer ${token}`
         },
     })
     if (!res||res.status!=200){ return false }
